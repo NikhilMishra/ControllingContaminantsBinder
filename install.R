896b77e198ec38e01e34a95c001019fc8580616b
NikhilMishra <mishranikh@gmail.com>
2019-05-31 15:23:18 -0700

    Update install.R

--- install.R
+++ install.R
@@ -2,3 +2,4 @@ install.packages(c("knitr", "ggplot2",
                    "tidyverse", "RColorBrewer", "reshape2", "ggpubr", "caTools", "rprojroot", "dplyr", "knitr", "kableExtra", "gridExtra"))
 source('http://bioconductor.org/biocLite.R')
 biocLite('phyloseq')
+biocLite("decontam")
